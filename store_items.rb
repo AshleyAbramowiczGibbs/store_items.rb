1de5856c54e11afa533ffe10c63ed752c6742c85
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-10-03 20:21:06 -0500

    create

---
+++ store_items.rb
@@ -0,0 +1,6 @@
+toothbrush1 = {:color => "pink", :brand => "crest", :price => 2}
+toothbrush2 = {:color => "green", :brand => "colgate", :price => 3}
+toothbrush2 = {:color => "green", :brand => "colgate", :price => 3}
+puts "this toothbrush is #{:color}. It's made by #{:brand} and costs #{:price}
+
+
