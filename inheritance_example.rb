db98e1839b50a87b02e1bc55addf4fc73f7ced0a
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-10-04 20:00:15 -0500

    practice adding inheritance

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,6 +1,6 @@
 class Vehicle
 
-  def initialize
+  def initialize(input_hash)
     @speed = 0
     @direction = 'north'
   end
@@ -19,22 +19,36 @@ class Vehicle
 end
 
 class Car < Vehicle
+  
+  def initialize(input_hash)
+  super
+    @fuel = input_hash [:fuel]
+    @make = input_hash [:make]
+    @model = input_hash [:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  
+  def initialize(input_hash)
+  super
+    @type = input_hash [:type]
+    @weight = input_hash [:weight]
+  end
 
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike1 = Bike.new()
+bike1 = Bike.new({type: "roadbike", weight: 40})
 bike1.ring_bell
 
-car1 = Car.new()
+car1 = Car.new({fuel: "regular", make: "jeep", model: "wrangler"})
 car1.honk_horn
 p car1
 p bike1
