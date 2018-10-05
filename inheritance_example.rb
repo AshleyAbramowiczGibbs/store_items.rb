4d390403013580f4aa8289762b7263b63f26d43b
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-10-04 19:38:56 -0500

    new edits

---
+++ inheritance_example.rb
@@ -0,0 +1,42 @@
+class Vehicle
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def accelerate
+    @speed += 10
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike1 = Bike.new()
+bike1.ring_bell
+
+car1 = Car.new()
+car1.honk_horn
+p car1
+p bike1
+
+
--- store_items.rb
+++ store_items.rb
@@ -4,8 +4,8 @@
 # puts "this toothbrush is #{toothbrush1[:color]}. It's made by #{toothbrush1[:brand]} and costs #{toothbrush1[:price] dollars}"
 
 class Toothbrushes
-attr_reader :color, :brand, :price, :instock
-attr_writer :color, :brand, :price, :instock
+  attr_reader :color, :brand, :price, :instock
+  attr_writer :color, :brand, :price, :instock
 
   def initialize(color, brand, price, instock)
     @color = color
@@ -14,17 +14,17 @@ attr_writer :color, :brand, :price, :instock
     @instock = instock
   end
 
-def info
-  puts "My toothbrush costs #{price}. It's made by #{brand} and it's #{color}."
-end
+  def info
+    puts "My toothbrush costs #{price}. It's made by #{brand} and it's #{color}."
+  end
 
-def on_sale
-  p @price / 2
-end
+  def on_sale
+    p @price / 2
+  end
 
-def instock_status
-  puts @instock
-end
+  def instock_status
+    puts @instock
+  end
 
 end
 
